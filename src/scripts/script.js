d5f415fbe20f51ee9740ad5566219cd7c4f325c5
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-08-25 15:14:41 -0300

    feat: cria script.js

---
+++ src/scripts/script.js
@@ -0,0 +1,43 @@
+class Api {
+  
+    static async listarClientes(){
+        const listaDeClientes = await fetch("https://atividade-api-clientes.herokuapp.com/clientes")
+        .then((response) => response.json())
+        .then((response) => {return response})
+        .catch((err) => console.log('This is the error' + err))
+
+        return listaDeClientes
+    }
+
+    static async cadastrarCliente(body){
+        const cliente = await fetch("https://atividade-api-clientes.herokuapp.com/clientes", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: body
+          })
+          .then(res => res.json())
+          .catch(err => console.log(err))
+      
+          return cliente
+    }
+
+    static async editarCliente(id, body){
+        const clienteEditado = await fetch(`https://atividade-api-clientes.herokuapp.com/clientes/${id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"},
+            body: body
+        })
+        .then(res => res.json())
+        .catch(err => console.log(err))
+    
+        return clienteEditado
+        
+    }
+
+    static async deletarCliente(id){
+
+    }
+
+}
+
+export {Api}
